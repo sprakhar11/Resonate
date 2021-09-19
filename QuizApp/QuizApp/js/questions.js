ffd5c94eb21556956c862d56efe31ec4ee83fb7a
sprakhar11 <prakhar.infinity@gmail.com>
2021-09-19 13:35:08 +0530

    commit

---
+++ QuizApp/QuizApp/js/questions.js
@@ -0,0 +1,115 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: " When does the ArrayIndexOutOfBoundsException occur?",
+    answer: "Run-time",
+    options: [
+      "Compile-time",
+      "Run-time",
+      "Not an error",
+      "Not an exception at all"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Assuming int is of 4bytes, what is the size of int arr[15];?",
+    answer: "60",
+    options: [
+      "15",
+      "19",
+      "11",
+      "60"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The data structure required to check whether an expression contains a balanced parenthesis is?",
+    answer: "Stack",
+    options: [
+      "Stack",
+      "Queue",
+      "Array",
+      "Tree"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The prefix form of A-B/ (C * D ^ E) is?",
+    answer: "	150 metres",
+    options: [
+      "	-/*^ACBDE",
+      "	-ABCD*^DE",
+      "	-A/B*C^DE",
+      "	-A/BC*^DE"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which data structure is used for implementing recursion?",
+    answer: "Stack",
+    options: [
+      "Queue",
+      "Stack",
+      "Array",
+      "List"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The data structure required for Breadth First Traversal on a graph is?",
+    answer: "Queue",
+    options: [
+      "Queue",
+      "Array",
+      "Stack",
+      "Tree"
+    ]
+  },
+  {
+    numb: 7,
+    question: "A queue follows which principle",
+    answer: "FIFO (First In First Out) principle",
+    options: [
+      "FIFO (First In First Out) principle",
+      "LIFO (Last In First Out) principle",
+      "Ordered array",
+      "Linear tree"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the time complexity of inserting at the end in dynamic arrays?",
+    answer: "Either O(1) or O(n)",
+    options: [
+      "O(1)",
+      "O(n)",
+      "Either O(1) or O(n)",
+      "O(logn)"
+    ]
+  },
+  {
+    numb: 9,
+    question: "A linear collection of data elements where the linear node is given by means of pointer is called?",
+    answer: "Linked list",
+    options: [
+      "Linked list",
+      "Node list",
+      "Unordered list",
+      "Primitive list"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The concatenation of two lists can be performed in O(1) time. Which of the following variation of the linked list can be used?",
+    answer: "Circular doubly linked list",
+    options: [
+      "Singly linked list",
+      "Doubly linked list",
+      "Circular doubly linked list",
+      "Array implementation of list"
+    ]
+  }
+ 
+
+];
---
+++ README.md
@@ -0,0 +1,2 @@
+# Resonate-Final
+By HashCode
